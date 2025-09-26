3460a9dea9187cc5cb117f881e96e93ef6469680
Yevhen <elsereno.ua@gmail.com>
2025-09-26 12:40:34 +0200

    2

--- src/main.js
+++ src/main.js
@@ -1,39 +1,39 @@
 /**
-* Функция для расчета выручки
-* @param purchase запись о покупке
-* @param _product карточка товара
-* @returns {number}
-*/
+ * Функция для расчета выручки
+ * @param purchase запись о покупке
+ * @param _product карточка товара
+ * @returns {number}
+ */
 function calculateSimpleRevenue(purchase, _product) {
    const discount = 1 - (purchase.discount / 100);
    return purchase.sale_price * purchase.quantity * discount;
 }
 
 /**
-* Функция для расчета бонусов
-* @param index порядковый номер в отсортированном массиве
-* @param total общее число продавцов
-* @param seller карточка продавца
-* @returns {number}
-*/
+ * Функция для расчета бонусов
+ * @param index порядковый номер в отсортированном массиве
+ * @param total общее число продавцов
+ * @param seller карточка продавца
+ * @returns {number}
+ */
 function calculateBonusByProfit(index, total, seller) {
    if (index === 0) {
-       return seller.profit * 0.15;
+       return +(seller.profit * 0.15).toFixed(2);
    } else if (index === 1 || index === 2) {
-       return seller.profit * 0.10;
+       return +(seller.profit * 0.10).toFixed(2);
    } else if (index === total - 1) {
        return 0;
    } else {
-       return seller.profit * 0.05;
+       return +(seller.profit * 0.05).toFixed(2);
    }
 }
 
 /**
-* Функция для анализа данных продаж
-* @param data
-* @param options
-* @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
-*/
+ * Функция для анализа данных продаж
+ * @param data
+ * @param options
+ * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
+ */
 function analyzeSalesData(data, options) {
    if (!data ||
        !Array.isArray(data.sellers) ||
@@ -60,19 +60,23 @@ function analyzeSalesData(data, options) {
    const productIndex = data.products.reduce((acc, product) => ({ ...acc, [product.sku]: product }), {});
    data.purchase_records.forEach(record => {
        const seller = sellerIndex[record.seller_id];
-       seller.sales_count += 1;
-       seller.revenue += record.total_amount - record.total_discount;
-       record.items.forEach(item => {
-           const product = productIndex[item.sku];
-           const cost = product.purchase_price * item.quantity;
-           const itemRevenue = calculateRevenue(item, product);
-           const itemProfit = itemRevenue - cost;
-           seller.profit += itemProfit;
-           if (!seller.products_sold[item.sku]) {
-               seller.products_sold[item.sku] = 0;
-           }
-           seller.products_sold[item.sku] += item.quantity;
-       });
+       if (seller) {
+           seller.sales_count += 1;
+           record.items.forEach(item => {
+               const product = productIndex[item.sku];
+               if (product) {
+                   const itemRevenue = calculateRevenue(item, product);
+                   seller.revenue += itemRevenue;
+                   const cost = product.purchase_price * item.quantity;
+                   const itemProfit = itemRevenue - cost;
+                   seller.profit += itemProfit;
+                   if (!seller.products_sold[item.sku]) {
+                       seller.products_sold[item.sku] = 0;
+                   }
+                   seller.products_sold[item.sku] += item.quantity;
+               }
+           });
+       }
    });
    sellerStats.sort((a, b) => b.profit - a.profit);
    sellerStats.forEach((seller, index) => {
@@ -90,7 +94,6 @@ function analyzeSalesData(data, options) {
        profit: +seller.profit.toFixed(2),
        sales_count: seller.sales_count,
        top_products: seller.top_products,
-       bonus: +seller.bonus.toFixed(2)
+       bonus: seller.bonus
    }));
 }
-
